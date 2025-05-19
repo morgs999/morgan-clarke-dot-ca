91836048abf23bdcc2ac95df58abfc0a3b928b4c
Morgan <109176008+morgs999@users.noreply.github.com>
2025-05-19 11:40:44 -0400

    init

---
+++ dataconnect-generated/js/default-connector/esm/index.esm.js
@@ -0,0 +1,7 @@
+
+export const connectorConfig = {
+  connector: 'default',
+  service: 'morgan-clarke-dot-ca',
+  location: 'northamerica-northeast2'
+};
+
---
+++ dataconnect-generated/js/default-connector/index.cjs.js
@@ -0,0 +1,7 @@
+
+const connectorConfig = {
+  connector: 'default',
+  service: 'morgan-clarke-dot-ca',
+  location: 'northamerica-northeast2'
+};
+exports.connectorConfig = connectorConfig;
