a4026d944341abc017983507c655388b5bb65e06
Morgan <109176008+morgs999@users.noreply.github.com>
2025-05-20 14:50:44 -0400

    added icons!

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,30 @@
 import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatCard } from '@angular/material/card'
+import { MatIconModule, MatIconRegistry } from '@angular/material/icon';
+import { DomSanitizer } from '@angular/platform-browser';
+import { provideHttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatSlideToggleModule, MatCard, MatIconModule],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
+  constructor(
+    private matIconRegistry: MatIconRegistry,
+    private domSanitizer: DomSanitizer
+  ) {
+    this.matIconRegistry.addSvgIcon(
+      'firebase',
+      this.domSanitizer.bypassSecurityTrustResourceUrl('./firebase.svg')
+    )
+  }
+
   back_end = [
     { name: 'Node JS' },
     { name: 'Express JS' },
