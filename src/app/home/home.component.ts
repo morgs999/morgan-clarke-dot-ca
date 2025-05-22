88321947ad4adc4d3dd62642c8d86f6dbab92d94
Morgan <109176008+morgs999@users.noreply.github.com>
2025-05-21 21:20:26 -0400

    landing page icons in

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,36 +1,30 @@
 import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MatSlideToggleModule } from '@angular/material/slide-toggle';
-import { MatCard } from '@angular/material/card'
-import { MatIconModule, MatIconRegistry } from '@angular/material/icon';
-import { DomSanitizer } from '@angular/platform-browser';
-import { provideHttpClient } from '@angular/common/http';
+import { MatCardModule } from '@angular/material/card';
+
+interface Icon {
+  name: string;
+  path: string;
+  color?: string;
+}
+
+const iconList: Icon[] = [
+  { name: 'Coding', path: './code.svg' },
+  { name: 'Music', path: './music.svg' },
+  { name: 'Podcasts', path: './podcast.svg' },
+  { name: 'My Blog', path: './blog.svg' },
+  { name: 'Writing', path: './write.svg' }
+]
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, MatSlideToggleModule, MatCard, MatIconModule],
+  imports: [CommonModule, MatCardModule],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
-export class HomeComponent {
-  constructor(
-    private matIconRegistry: MatIconRegistry,
-    private domSanitizer: DomSanitizer
-  ) {
-    this.matIconRegistry.addSvgIcon(
-      'firebase',
-      this.domSanitizer.bypassSecurityTrustResourceUrl('./firebase.svg')
-    )
-  }
 
-  back_end = [
-    { name: 'Node JS' },
-    { name: 'Express JS' },
-    { name: 'Python' },
-    { name: 'C++' },
-    { name: 'Restful APIs' },
-    { name: 'Databases' }
-  ]
+export class HomeComponent {
+  iconList = iconList;
 }
